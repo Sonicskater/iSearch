2109ed5727ca6c2ddaa7be2c9981f50f53cd7db8
Devon Hockley <devon@hockley.ca>
2020-04-14 15:06:07 -0600

    Completed 1 test, also added some dependencies to fix test runner exceptions

--- app/src/main/java/com/devon/isearch/viewmodel/SearchModel.kt
+++ app/src/main/java/com/devon/isearch/viewmodel/SearchModel.kt
@@ -1,12 +1,21 @@
 package com.devon.isearch.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.devon.isearch.model.Movie
 
 class SearchModel : ViewModel(), ISearchModel {
+    private val _movies: MutableLiveData<List<Movie>> = MutableLiveData()
+
+    // initialize LiveData, doesn't seem like i can provide a initial value in constructor
+    init {
+        _movies.value = listOf()
+    }
+
     override val movies: LiveData<List<Movie>>
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        get() = _movies
+
     override var searchString: String
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
         set(value) {}
--- app/src/test/java/com/devon/isearch/SearchModelTest.kt
+++ app/src/test/java/com/devon/isearch/SearchModelTest.kt
@@ -1,14 +1,22 @@
 package com.devon.isearch
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.devon.isearch.viewmodel.ISearchModel
 import com.devon.isearch.viewmodel.SearchModel
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 
 class SearchModelTest {
     lateinit var searchModel: ISearchModel
+
+    // jvmField needed otherwise the test runner gets upset: https://proandroiddev.com/fix-kotlin-and-new-activitytestrule-the-rule-must-be-public-f0c5c583a865
+    // rule needed to mock the task executor for LiveData, otherwise throws fatal exception
+    @Rule @JvmField
+    var rule = InstantTaskExecutorRule()
+
     @Before
     fun setup(){
         searchModel = SearchModel()
