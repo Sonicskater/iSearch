a335c3545e01705a41aeed1bb5a6b1fffd4cbbdc
Devon Hockley <devon@hockley.ca>
2020-04-15 16:15:23 -0600

    Switched to Android Jetpack navigation (single activity arch), also switched to DayNight android theme for hopeful dark mode support.

--- app/src/androidTest/java/com/devon/isearch/SearchActivityTest.kt
+++ app/src/androidTest/java/com/devon/isearch/SearchActivityTest.kt
@@ -1,19 +1,11 @@
 package com.devon.isearch
 
-import androidx.test.espresso.Espresso.onData
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import androidx.test.rule.ActivityTestRule
 
-import com.devon.isearch.view.MainActivity
-import kotlinx.android.synthetic.main.activity_main.view.*
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.core.AllOf.allOf
+import com.devon.isearch.view.HostActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -30,19 +22,13 @@ import org.junit.Rule
 class SearchActivityTest {
 
     @get:Rule
-    var activityRule: ActivityTestRule<MainActivity>
-            = ActivityTestRule(MainActivity::class.java)
+    var activityRule: ActivityTestRule<HostActivity>
+            = ActivityTestRule(HostActivity::class.java)
 
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.devon.isearch", appContext.packageName)
-    }
-
-    @Test
-    fun recyclerViewEmpty(){
-        var x =onView(withId(R.id.test_id))
-        x.perform().check(matches(withText("Hello World!")))
     }
 }
--- app/src/main/java/com/devon/isearch/view/HostActivity.kt
+++ app/src/main/java/com/devon/isearch/view/HostActivity.kt
@@ -2,12 +2,15 @@ package com.devon.isearch.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import com.devon.isearch.R
 
-class MainActivity : AppCompatActivity() {
+class HostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
+
     }
 }
