a5fe6533735a890535d518d95b5b4bdc529959f6
Devon Hockley <devon@hockley.ca>
2020-04-15 18:02:14 -0600

    setup mocks for testing ui

--- app/src/androidTest/java/com/devon/isearch/MovieListFragmentTest.kt
+++ app/src/androidTest/java/com/devon/isearch/MovieListFragmentTest.kt
@@ -2,23 +2,41 @@ package com.devon.isearch
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.devon.isearch.model.types.Movie
+import com.devon.isearch.mocks.MockSearchModel
 import com.devon.isearch.view.MovieListFragment
-import org.hamcrest.CoreMatchers.allOf
-import org.hamcrest.CoreMatchers.instanceOf
-import org.hamcrest.Matchers.hasEntry
+import com.devon.isearch.viewmodel.ISearchModel
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.Assume.assumeTrue
+import org.junit.Before
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
 
 @RunWith(AndroidJUnit4::class)
 class MovieListFragmentTest {
 
     // Test that the recycler view is initialized correctly to an empty state
+    @Before
+    fun setup(){
+        stopKoin()
+        startKoin {
+            modules(module {
+                viewModel<ISearchModel> { MockSearchModel(
+                    mutableListOf(
+                        Movie("Antman")
+                    )
+                ) }
+            })
+        }
+    }
     @Test
     fun testInitialState(){
         val scenario = launchFragmentInContainer<MovieListFragment>(fragmentArgs = null, factory = null)
---
+++ app/src/androidTest/java/com/devon/isearch/mocks/MockSearchModel.kt
@@ -0,0 +1,23 @@
+package com.devon.isearch.mocks
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.devon.isearch.model.types.Movie
+import com.devon.isearch.viewmodel.ISearchModel
+
+class MockSearchModel(private val all_movies: List<Movie>) :  ISearchModel(){
+    private val _movies: MutableLiveData<List<Movie>> = MutableLiveData(listOf())
+    override val movies: LiveData<out List<Movie>>
+        get() = _movies
+    override var searchString: String = ""
+        get() = field
+        set(value) {
+            field = value
+            if (value.isNotEmpty()) {
+                _movies.value = all_movies.filter { it.name.startsWith(value) }
+            }else{
+                _movies.value = listOf()
+            }
+        }
+
+}
